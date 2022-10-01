6c4515d0f7d6d8a7c737f8cc532acb1c66bfc027
Chris Jonak <chris@ironbrush.com>
2022-10-01 11:05:21 -0500

    First set up numbers display, operators display and erase first numbers off screen

--- script.js
+++ script.js
@@ -1,39 +1,88 @@
 
-const display = document.getElementById('calc-disp');
+//Dont think I need this ------>  const display = document.getElementById('calc-disp');
 const btns = document.getElementsByClassName('buttons');
 const clear = document.getElementById('C');
 const numbers = document.getElementById('numbDisplay');
-let numbArray = [];
-let numbString;
+const operatorDisplay = document.getElementById('operatorDisplay');
+const operators = document.querySelectorAll('.operators');
+const calculate = document.getElementById('calculate');
+
+let numbArrayOne = [];
+let numbStringOne;
+
+let numbArrayTwo = [];
+let numbStringTwo;
+
+let numbArrayThree = [];
+let numbStringThree;
+//------------------------------------------------------
 
 
+
+
+
+
+
+// LOOP AND FUNCTION FOR CONCATENATING NUMBERS ONTO DISPLAY
 for (const buttons of btns) {
     buttons.addEventListener('click', function () {
-        numbArray.push(buttons.innerHTML);
-        numbString = numbArray.join('');
+        numbArrayOne.push(buttons.innerHTML);
+        numbStringOne = numbArrayOne.join('');
 
-        if (numbString.length <= 12) {
-            numbers.innerHTML = numbString;
+        if (numbStringOne.length <= 12) {
+            numbers.innerHTML = numbStringOne;
         }
         else {
-            numbString = numbString.substring(0, 12);
-            numbers.innerHTML = numbString;
-            console.log(numbString);
+            numbStringOne = numbStringOne.substring(0, 12);
+            numbers.innerHTML = numbStringOne;
+            console.log(numbStringOne);
         }
 
     });
 }
+//----------------------------------------------------------
 
 
 
+// CLEAR BUTTON EVENT LISTENER FUNCTION----------
+clear.addEventListener('click', clearDisplay);
 
-
-clear.addEventListener('click', function() {
+function clearDisplay() {
     numbers.innerHTML = '';
-    numbArray.length = 0;
+    numbArrayOne.length = 0;
+    operatorDisplay.innerHTML = '';
+    numbArrayTwo.length = 0;
+    numbers.style.display = "inline";
+}
+//----------------------------------------------
+
+
+
+
+
+
+// LOOP AND FUNCTION FOR OPERATORS ONTO DISPLAY, REMOVES FIRST SET OF NUMBERS FROM DISPLAY AND PROHIBITS CLICK OF ANOTHER OPERATOR OR NUMBER
+operators.forEach(item => {
+    item.addEventListener('click', function() {
+    numbArrayTwo.push(item.innerHTML);
+    numbStringTwo = numbArrayTwo.join('');
+    operatorDisplay.innerHTML = numbStringTwo;
+    numbers.style.display = "none";
+    console.log(numbStringTwo);
 });
+});
+        
 
     
+       
+
+//----------------------------------------------------
+
+
+
+
+
+
 
 
 
@@ -41,11 +90,8 @@ clear.addEventListener('click', function() {
 //------------------------------------------------------
 // MATH BELOW
 
-
-
-
 function addNumbers(a,b) {
-    return a + b;
+    return numbStringOne + numbStringThree;
 }
 function subtractNumbers(a,b) {
     return a - b;
@@ -62,7 +108,7 @@ function divideNumbers(a,b) {
 //if(str.length > 10) str = str.substring(0,10);
 
 
-// Type in number and has limit to how many times, so numbers fit in diplay
+// Type in number and has limit to how many times, so numbers fit in diplay -----FINISHED
 // When you click an operator it removes eventListener from all buttons
 // Ability to type in second string of numbers 
 // function takes two the two strings of numbers and calculates them based on chosen operator, once = is clicked.
